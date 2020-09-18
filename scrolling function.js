ddf306daa0fab154b3e86b7ca1f20bd171e6a6f7
mrlaboratory <admin@mrlaboratory.com>
2020-09-18 14:30:31 +0600

    scrolling function

---
+++ scrolling function.js
@@ -0,0 +1,11 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+   
+  } else {
+   
+
+  }
+  prevScrollpos = currentScrollPos;
+}
