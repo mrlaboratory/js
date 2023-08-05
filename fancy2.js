270a120ff14b3d5ccfb3a3b5775c97a9ab5e4e19
Md Mijanur Rahaman <58909573+mrlaboratory@users.noreply.github.com>
2023-08-05 11:26:26 +0600

    Update fancy2.js

--- fancy2.js
+++ fancy2.js
@@ -53,12 +53,12 @@ $("#result").append(fancyText);
 });
 
 $(function(){
-var intv = setInterval(function(){ $(".copybutton").html('Copy'); }, 2000);
-$("body").on('click',".copybutton",function() { 
-$(".copybutton").html('Copy');
-$(this).html('Copied'); 
-clearInterval(intv);
-});
+    var intv = setInterval(function(){ $(".copybutton").html('Copy'); }, 2000);
+    $("body").on('click', ".copybutton", function() { 
+        $(".copybutton").html('Copy');
+        $(this).html('Copied'); 
+        clearInterval(intv);
+    });
 });
 
 var clipboard = new ClipboardJS('.copybutton');
