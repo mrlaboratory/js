21050772cb6b975c3eb7b02de2d1918a0c9221de
Md Mijanur Rahaman <58909573+mrlaboratory@users.noreply.github.com>
2025-02-27 22:33:48 +0600

    Create info.js

---
+++ info.js
@@ -0,0 +1,20 @@
+/**
+ * Developer Credit Script
+ * 
+ * Developed by: Md Mijanur Rahaman
+ * LinkedIn: https://www.linkedin.com/in/mijanurrahaman
+ * GitHub: https://github.com/mrlaboratory
+ * Website: https://mrlaboratory.github.io
+ * Business: Bismillah IT House, Shantirhat, Patiya, Chittagong
+ * Business Website: https://bismillahithouse.com/
+ * WhatsApp: https://wa.me/8801854650673
+ * Contact: mrlaboratory23@gmail.com | +8801854650673
+ * 
+ * Description: This script displays a developer credit in the console.
+ */
+
+(function() {
+    const creditMessage = `\n%cDeveloped by: Md Mijanur Rahaman\n%cLinkedIn: https://www.linkedin.com/in/mijanurrahaman\nGitHub: https://github.com/mrlaboratory\nWebsite: https://mrlaboratory.github.io\nBusiness: Bismillah IT House, Shantirhat, Patiya, Chittagong\nBusiness Website: https://bismillahithouse.com/\nWhatsApp: https://wa.me/8801854650673\nContact: mrlaboratory23@gmail.com | +8801854650673\n`;
+    
+    console.log(creditMessage, 'color: blue; font-size: 16px;', 'color: green; font-size: 14px;');
+})();
